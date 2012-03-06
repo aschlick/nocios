f5fb46de0870746e4fca5a6d17253f615616007c
Alexander Schlicker <alex.schlicker@ccisystems.com>
2012-03-06 10:07:28 -0600

    fixes some nonsense

--- app/helpers/bootstrap_helper.rb
+++ app/helpers/bootstrap_helper.rb
@@ -1,6 +1,5 @@
 module BootstrapHelper
   def horizontal_form_field(item, lbl)
-    Rails.logger.info("label: #{lbl}")
     content_tag :div, :class => 'control-group' do 
       content_tag(:label, lbl, :class => 'control-label')+
       content_tag(:div, :class => 'controls') do 
