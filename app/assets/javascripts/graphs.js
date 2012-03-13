f09f4fcb064b89545ba7c92c8e8b7010c287d7e7
Alexander Schlicker <alex.schlicker@ccisystems.com>
2012-03-13 11:33:49 -0500

    graphs use localtime instead of utc

--- app/assets/javascripts/graphs.js
+++ app/assets/javascripts/graphs.js
@@ -1,14 +1,18 @@
 $(function() {
-//check level graph
-if($("#check_graph").length > 0){
-  host_id = $("#check_graph").data('host_id');
-  check_id = $("#check_graph").data('check_id');
-  $.getJSON("/hosts/"+host_id+"/checks/"+check_id+"/graph.json",function(data){
-    createSingleLineChart(data);
-  });
-}
-//network level graphs
-
+  Highcharts.setOptions({
+    global: {
+      useUTC: false
+    }
+  })
+  //check level graph
+  if($("#check_graph").length > 0){
+    host_id = $("#check_graph").data('host_id');
+    check_id = $("#check_graph").data('check_id');
+    $.getJSON("/hosts/"+host_id+"/checks/"+check_id+"/graph.json",function(data){
+      createSingleLineChart(data);
+    });
+  }
+  //network level graphs
 });
 
 function createSingleLineChart(data) {
